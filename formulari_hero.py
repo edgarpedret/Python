cd0ea50ecdfe51f076152d3b156db1e5c92445ad
Edgar <158268605+edgarpedret@users.noreply.github.com>
2025-02-19 14:42:46 +0100

    Update and rename hero.py to formulari_hero.py

---
+++ formulari_hero.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from jinja2 import Environment, FileSystemLoader
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def formulari():
+    if request.method == 'POST':
+
+        tipus = request.form['tipus']
+        nom = request.form['nom']
+        atac = request.form['atac']
+        defensa = request.form['defensa']
+        moviment = request.form['moviment']
+        cos = request.form['cos']
+        ment = request.form['ment']
+        habilitats = request.form['habilitats']
+
+        #  Aqui retorna la pagina html amb el resultat
+        return render_template("retorno_hero.html", tipus=tipus, nom=nom, atac=atac,
+                               defensa=defensa, moviment=moviment, cos=cos,
+                               ment=ment, habilitats=habilitats)
+
+    else:# Aqui et dona la pagina html amb el formulari
+        environment = Environment(loader=FileSystemLoader("templates/"))
+        template = environment.get_template("formulari1.html")
+        return template.render()
+
+if __name__ == '__main__':
+    app.run(debug=True)
